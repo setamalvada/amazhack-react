6816e43cd776c1c297552021bd76ca7e8e85eaab
Carol Alonso <c.alonsolp@gmail.com>
2020-10-09 20:44:50 +0200

    task: basic directions

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import React from "react";
-import "./App.css";
-import Login from "./components/login/Login";
-import { Switch, Route } from "react-router-dom";
-import ProductList from "./components/product-list/ProductList";
+import './App.css';
+import React from 'react';
+import { Redirect, Route, Switch } from 'react-router-dom';
+import Login from './components/login/Login';
+import ProductList from './components/product-list/ProductList';
 
 function App() {
   return (
@@ -10,8 +10,9 @@ function App() {
       {/* We want a header that will appear in every screen */}
       <Switch>
         <Route path="/products" component={ProductList} />{" "}
-        {/* We don't wan't non-authenticated users to access this route! */}
+        {/* We don't wan't non-authenticated users to access the previous route! */}
         <Route path="/" component={Login} />
+        <Redirect to="/products" />
       </Switch>
     </div>
   );
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,6 @@
+import "./Header.css";
+import React from "react";
+
+export default function Header() {
+  return <div className="Header"></div>;
+}
